2e582bed9a85ecd4361203c6052e5b8121862e12
George Udonte <georgeudonte@gmail.com>
2023-02-06 18:29:25 +0100

    hero page: developed ui

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+      Button
+    </div>
+  )
+}
+
+export default Button
+Button
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Clients from "./Clients";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+  Navbar,
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  FeedbackCard,
+  Footer,
+  CTA,
+  Clients,
+  GetStarted,
+  Hero,
+  Stats,
+  Testimonials
+}
